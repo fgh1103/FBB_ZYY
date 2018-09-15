2487f9e2d574b9fdb45f5cd21b32573e272f6392
jiruixing <18357126171@163.com>
2018-09-15 17:28:33 +0800

    goods pages update

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -1,47 +1,60 @@
 Page({
   data: {
-    category: [
-      { name: '果味', id: 'guowei' },
-      { name: '蔬菜', id: 'shucai' },
-      { name: '炒货', id: 'chaohuo' },
-      { name: '点心', id: 'dianxin' },
-      { name: '粗茶', id: 'cucha' },
-      { name: '淡饭', id: 'danfan' }
-    ],
-    goodsList: [
-      {
-        name:'商品1',
-        id:1,
-        detail:'详情描述',
-        price:10,
-        banner:'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
-        detail: [
-          {
-            thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
-           name: 'ss' }, 
-          {
-            thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg', 
-          name: 'ss2' }
-          ],
+    category: [{
+        name: '狗粮',
+        id: 'guowei'
       },
       {
-        name: '商品2',
-        id: 2,
-        detail: '详情描述2',
-        price: 10,
-        banner: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
-        detail: [
-          {
-            thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
-            name: 'ss'
-          },
-          {
-            thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
-            name: 'ss2'
-          }
-        ],
+        name: '猫粮',
+        id: 'shucai'
+      },
+      {
+        name: '狗盆',
+        id: 'chaohuo'
+      },
+      {
+        name: '猫盆',
+        id: 'dianxin'
+      },
+      {
+        name: '猫砂',
+        id: 'cucha'
+      },
+      {
+        name: '狗窝',
+        id: 'danfan'
       }
-      ],
+    ],
+    goodsDetail: {
+      "id": 1,
+      banner: "http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg",
+      title: "description",
+      list: [{
+          id: "productId",
+          thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+          name: 'ss2',
+          price: 1.0
+        },
+        {
+          id: "productId2",
+          thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+          name: 'ss2',
+          price: 1.0
+        },
+        {
+          id: "productId3",
+          thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+          name: 'ss2',
+          price: 1.0
+        },
+        {
+          id: "productId4",
+          thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+          name: 'ss2',
+          price: 1.0
+        }
+      ]
+    },
     curIndex: 0,
     isScroll: false,
     toView: 'guowei'
@@ -63,13 +76,46 @@ Page({
     this.setData({
       isScroll: true
     })
-    setTimeout(function () {
+    setTimeout(function() {
       self.setData({
         toView: e.target.dataset.id,
         curIndex: e.target.dataset.index
+      });
+      //重新请求获取数据
+      self.setData({
+        goodsDetail: {
+          "id": 1,
+          banner: "http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg",
+          title: "description",
+          list: [{
+              id: "productId",
+              thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+              name: '刷新',
+              price: 1.0
+            },
+            {
+              id: "productId2",
+              thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+              name: '刷新',
+              price: 2.0
+            },
+            {
+              id: "productId3",
+              thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+              name: '刷新',
+              price: 1.0
+            },
+            {
+              id: "productId4",
+              thumb: 'http://d.ifengimg.com/mw978_mh598/p0.ifengimg.com/2018_36/ec148994-862f-4210-a32f-c92878dfd38d_2FAAC0E33E170C4950CC2D30449E928040011A73_w1024_h576.jpg',
+              name: '刷新',
+              price: 1.0
+            }
+          ]
+        }
       })
     }, 0)
-    setTimeout(function () {
+    setTimeout(function() {
       self.setData({
         isScroll: false
       })
